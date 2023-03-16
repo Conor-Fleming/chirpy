7b550e304a053c0bc14c55ae43bc3e3d8bf83b32
Conor-Fleming <conorfleming03@gmail.com>
2023-03-15 18:31:51 -0700

    readiness handler

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"io"
 	"log"
 	"net/http"
 )
@@ -8,6 +9,7 @@ import (
 func main() {
 	mux := http.NewServeMux()
 	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.HandleFunc("/healthz", healthzHandler)
 	corsMux := corsMiddleware(mux)
 
 	server := &http.Server{
@@ -28,4 +30,10 @@ func corsMiddleware(next http.Handler) http.Handler {
 		}
 		next.ServeHTTP(w, r)
 	})
+}
+
+func healthzHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	io.WriteString(w, "OK")
 }
